082467df3d55f0f0a602f93ad589ec148e2e2a74
unknown <sahildesouza@gmail.com>
2024-07-01 12:02:35 +0100

    HibernusV1

---
+++ Debug/hibernation.d
@@ -0,0 +1,54 @@
+# FIXED
+
+hibernation.obj: ../hibernation.c
+hibernation.obj: D:/ti/ccstheia140/ccs/ccs_base/msp430/include/msp430.h
+hibernation.obj: D:/ti/ccstheia140/ccs/ccs_base/msp430/include/msp430fr5994.h
+hibernation.obj: D:/ti/ccstheia140/ccs/ccs_base/msp430/include/in430.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+hibernation.obj: D:/ti/ccstheia140/ccs/ccs_base/msp430/include/legacy.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdio.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdarg.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h
+hibernation.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h
+hibernation.obj: ../hibernation.h
+
+../hibernation.c:
+
+D:/ti/ccstheia140/ccs/ccs_base/msp430/include/msp430.h:
+
+D:/ti/ccstheia140/ccs/ccs_base/msp430/include/msp430fr5994.h:
+
+D:/ti/ccstheia140/ccs/ccs_base/msp430/include/in430.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+D:/ti/ccstheia140/ccs/ccs_base/msp430/include/legacy.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdio.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdarg.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h:
+
+../hibernation.h:
+
---
+++ Debug/msp430fr599x_1.d
@@ -0,0 +1,27 @@
+# FIXED
+
+msp430fr599x_1.obj: ../msp430fr599x_1.c
+msp430fr599x_1.obj: D:/ti/ccstheia140/ccs/ccs_base/msp430/include/msp430.h
+msp430fr599x_1.obj: D:/ti/ccstheia140/ccs/ccs_base/msp430/include/msp430fr5994.h
+msp430fr599x_1.obj: D:/ti/ccstheia140/ccs/ccs_base/msp430/include/in430.h
+msp430fr599x_1.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+msp430fr599x_1.obj: D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+msp430fr599x_1.obj: D:/ti/ccstheia140/ccs/ccs_base/msp430/include/legacy.h
+msp430fr599x_1.obj: ../hibernation.h
+
+../msp430fr599x_1.c:
+
+D:/ti/ccstheia140/ccs/ccs_base/msp430/include/msp430.h:
+
+D:/ti/ccstheia140/ccs/ccs_base/msp430/include/msp430fr5994.h:
+
+D:/ti/ccstheia140/ccs/ccs_base/msp430/include/in430.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+D:/ti/ccstheia140/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+D:/ti/ccstheia140/ccs/ccs_base/msp430/include/legacy.h:
+
+../hibernation.h:
+
